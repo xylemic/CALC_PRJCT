e65078302f965851d476d627b469279b754f4ab7
xylemic <junesveryown6ix@gmail.com>
2024-04-30 09:59:27 +0100

    first commit for this project

---
+++ calc.js
@@ -0,0 +1,60 @@
+
+"use strict";
+
+let currentNumber = "";
+let previousNumber = null;
+let operation = null;
+
+function handleNumber(number) {
+    currentNumber += number;
+    document.getElementById("output").value = currentNumber;
+}
+
+function handleOperation(op) {
+    if (currentNumber === "") return;
+    previousNumber = parseFloat(currentNumber);
+    currentNumber = "";
+    operation = op;
+}
+
+function calculate() {
+
+
+    if (previousNumber === null || currentNumber === "" || operation === null) return;
+
+    let result;
+    const NUM1 = previousNumber;
+    const NUM2 = parseFloat(currentNumber);
+
+    switch (operation) {
+        case "+":
+            result = NUM1 + NUM2;
+            break;
+        case "-":
+            result = NUM1 - NUM2;
+            break;
+        case "×":
+            result = NUM1 * NUM2;
+            break;
+        case "%":
+            result = NUM1 % NUM2;
+            break;
+        case "÷":
+            if (NUM2 === 0) {
+                // alert("Division by zero is not allowed!");
+                document.getElementById("output").value = "Division by zero is not allowed!";
+                return;
+            }
+            result = NUM1 / NUM2;
+            break;
+            default:
+                return;
+    }
+
+    currentNumber = result.toString();
+    previousNumber = null;
+    operation = null;
+    document.getElementById("output").value = currentNumber;
+}
+
+document.getElementById("output").value = "0";
